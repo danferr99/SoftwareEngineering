0a447ed8283ec09427bfd0283009267b7d262d18
Daniel dos Reis Teodoro Ferreira <daniel.ferreira@zema.com>
2025-05-05 14:35:21 -0300

    Estudos

---
+++ ENCAPSULAMENTO/Pessoa.java
@@ -0,0 +1,31 @@
+//Exemplo prático encapsulamento
+import java.util.Random;
+    //Classe
+    public class Pessoa {    
+    //Atributos
+    private String nome;
+    private int codigo_identificador;
+    private Random aleatorio;   
+    //Métodos
+    public Pessoa (String nome){
+         aleatorio = new Random();
+         this.setNome(nome);
+         this.codigo_identificador = 10_000_000 + aleatorio.nextInt(90_000_000);
+    }
+    private void setNome (String nome) {
+         this.nome = nome;
+    }  
+    public String getNome () {
+         return this.nome;
+    }
+    public int getCodigoIdentificador (){
+         return this.codigo_identificador;
+    }   
+    public static void main(String args[]){
+        Pessoa p1 = new Pessoa("Teste A");
+        Pessoa p2 = new Pessoa("Teste B");
+        System.out.println("[Pessoa 1] nome: "+p1.getNome()+", Código Identificador: "+p1.getCodigoIdentificador());
+        System.out.println("[Pessoa 2] nome: "+p2.getNome()+", Código Identificador: "+p2.getCodigoIdentificador());
+
+    }
+}
